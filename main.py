c519519e5636872ae461044c75a0bc5ee3f51c4e
Michael Feneberg <117763927+MMFeneberg@users.noreply.github.com>
2022-11-11 08:39:26 -0500

    Name, Age, Job Printer

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+job = input("Enter your job: ")
+print("Your name is: " + name + "\nYour age is: " + age + "\nYour job is: " + job)
